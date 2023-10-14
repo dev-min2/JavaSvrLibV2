c016f90560cd3354180bb49078108604646c2d5c
Kmin <alsry4547@gmail.com>
2023-10-14 22:02:02 +0900

    한글깨짐 수정

--- src/CoreAcitive/RequestMapping.java
+++ src/CoreAcitive/RequestMapping.java
@@ -2,7 +2,8 @@ package CoreAcitive;
 
 import java.lang.annotation.*;
 
-// 패킷 및 메소드에 적용.
+
+//패킷 및 메소드에 적용..
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface RequestMapping {
